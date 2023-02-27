1a3218fa9380e6558bcb27d22187db499234bbad
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-02-27 15:04:21 -0500

    ci: implement schema/00_user_levels.sql

---
+++ server/db/schema/00_user_levels.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS user_levels CASCADE;
+
+CREAT TABLE user_levels (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(250) NOT NULL,
+  description VARCHAR(2000) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW (),
+  updated_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
