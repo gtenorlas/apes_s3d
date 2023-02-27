f9dfa53d294dc300777163e9f2243b59136d4aa6
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-02-27 10:22:01 -0500

    ci: implement schema/07_tournaments.sql

---
+++ server/db/schema/07_tournaments.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS tournaments CASCADE;
+
+CREATE TABLE
+  tournaments (
+    id SERIAL PRIMARY KEY NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME NOT NULL,
+    description VARCHAR(4000) NOT NULL,
+    title VARCHAR(1000) NOT NULL,
+    prizes VARCHAR(1000) NOT NULL,
+    sponsors VARCHAR(500) NOT NULL,
+    tournament_type_id INT NOT NULL REFERENCES tournamens (id) ON DELETE CASCADE,
+    thumbnail VARCHAR(250) NOT NULL,
+    winner INT REFERENCES users (id) ON DELETE CASCADE,
+    runner_up INT REFERENCES users (id) ON DELETE CASCADE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW (),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+  );
