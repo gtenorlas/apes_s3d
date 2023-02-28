4cf45a8f3c0475fff738b3062698dce4c3ede393
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-02-28 18:43:48 -0500

    ci: implement seeds/09_matches.sql

---
+++ server/db/seeds/09_matches.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+  matches (
+    tournament_id,
+    player1,
+    player2,
+    war_id,
+    round,
+    winner,
+    loser
+  )
+VALUES
+(2, 4, 5, 1, 1, 5, 4),
+  (2, 6, 7, 1, 1, 5, 6),
+  (2, 5, 6, 2, 2, 6, 5);
