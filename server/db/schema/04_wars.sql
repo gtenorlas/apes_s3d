92225eecde7a6b55cec05e726ade3d519c30ad85
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-02-28 09:54:28 -0500

    ci: implement seeds/04_wars.sql

--- server/db/schema/04_wars.sql
+++ server/db/schema/04_wars.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS wars CASCADE;
 CREATE TABLE
   wars (
     id SERIAL PRIMARY KEY NOT NULL,
+    group_id INT NOT NULL REFERENCES groups(id) ON DELETE CASCADE,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     number_of_days INT NOT NULL,
---
+++ server/db/seeds/04_wars.sql
@@ -0,0 +1,26 @@
+INSERT INTO
+  wars (
+    group_id,
+    start_date,
+    end_date,
+    number_of_days,
+    month,
+    thumbnail
+  )
+VALUES
+  (
+    1,
+    '2023-02-01 08:00:00 PST',
+    '2023-02-02 08:00:00 PST',
+    1,
+    2,
+    'Squad1_02022023'
+  ),
+  (
+    1,
+    '2023-02-02 08:00:00 PST',
+    '2023-02-04 08:00:00 PST',
+    2,
+    2,
+    'Squad1_02042023'
+  );
