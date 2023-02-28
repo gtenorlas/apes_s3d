b6a283559a088446de371e7cc15468d026fe5b1b
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-02-27 21:41:08 -0500

    ci: implement seeds/00_user_levels.sql

---
+++ server/db/seeds/00_user_levels.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+  user_levels (name, description)
+VALUES
+  ('Owner', 'Highest Admin'),
+  ('Admin', 'Admin'),
+  ('Captain', 'Captain of squads'),
+  ('Senior', 'Trusted members'),
+  ('Member', 'Regular member'),
+  ('Guest', 'Guest or alliances');
