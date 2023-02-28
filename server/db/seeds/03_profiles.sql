3948bba84b3c0c3c306cc553294b61c330a18a7e
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-02-28 09:28:17 -0500

    ci: implement seeds/03_profiles.sql

---
+++ server/db/seeds/03_profiles.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+  profiles (user_id, group_id, name, s3d_id, is_default)
+VALUES
+  (
+    1,
+    1,
+    'Jacmindster 2',
+    '483234nsfnkenrkr43',
+    true
+  ),
+  (
+    1,
+    2,
+    'Sniperdaw',
+    '483234nsfnerrereakenrkr43',
+    false
+  ),
+  (2, 1, 'Cardo', '434383234nsfnkenrkr43', false),
+  (3, 1, 'Guest933', '4343483234nsfnkenrkr43', true);
