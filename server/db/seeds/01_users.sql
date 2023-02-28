94ea1f0e6b9011f99c43797de25e2f5b29c2ab1d
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-02-28 09:08:24 -0500

    ci: implement seeds/01_users.sql

---
+++ server/db/seeds/01_users.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+  users (
+    user_level_id,
+    email,
+    password,
+    first_name,
+    last_name,
+    gender
+  )
+  VALUES(1,'jack@nowhere.com','abc','Jack','Mindster','male'),
+  VALUES(5,'george@abc.com','abc','George','Maul','male');
+  VALUES(6,'guest@abc.com','abc','Guest','Star','female');
