f0b1eff869cfad6bc6c1ba80bbe0effbca2d137e
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-03-06 20:37:33 -0500

    feature: add logo

---
+++ apes-s3d/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.scss";
+import { Box } from "@mui/material";
+import { ClassNames } from "@emotion/react";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Box component="span">
+          <img src="images/logo.png" alt="logo" />
+        </Box>
+      </div>
+    </>
+  );
+}
+
+export default App;
