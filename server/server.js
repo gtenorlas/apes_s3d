b3eae6f43e52de0193327c893bae3b94bad7f2ba
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-10-22 20:09:30 -0400

    install cors by using npm install cors

--- server/server.js
+++ server/server.js
@@ -9,11 +9,19 @@ app.use(cookieSession({
   keys: [process.env.SESSION_SECRET_KEY]
 }));
 
+//------------------------------------
+const cors = require("cors");
+app.use(cors());
+//------------------------------------
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const userlevelsRoutes = require('./routes/userlevels-api')
+
+//create endpoints
+app.use('/api/userlevels',userlevelsRoutes);
 
 app.listen(process.env.PORT || 8080,()=>{
   console.log("Server running");
