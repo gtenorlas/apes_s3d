f61a135191771583ec114e610f80bb9a9596ef30
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-11-05 18:25:32 -0500

    implement groups-api

---
+++ server/routes/groups-api.js
@@ -0,0 +1,75 @@
+const express = require('express');
+
+const groupsDAO = require('/db/dao/groupsDAO'); // Import the module containing CRUD functions
+
+const app = express();
+
+
+// Create a new group
+app.post('/', async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    const newGroup = await groupsDAO.createGroup(name, description);
+    res.json(newGroup);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Retrieve all groups
+app.get('/', async (req, res) => {
+  try {
+    const groups = await groupsDAO.getAllGroups();
+    res.json(groups);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Retrieve a specific group by ID
+app.get('/:id', async (req, res) => {
+  try {
+    const groupId = parseInt(req.params.id);
+    const group = await groupsDAO.getGroupById(groupId);
+    if (group) {
+      res.json(group);
+    } else {
+      res.status(404).json({ error: 'Group not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Update a group by ID
+app.put('/:id', async (req, res) => {
+  try {
+    const groupId = parseInt(req.params.id);
+    const { name, description } = req.body;
+    const updatedGroup = await groupsDAO.updateGroup(groupId, name, description);
+    if (updatedGroup) {
+      res.json(updatedGroup);
+    } else {
+      res.status(404).json({ error: 'Group not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Soft delete a group by ID
+app.delete('/:id', async (req, res) => {
+  try {
+    const groupId = parseInt(req.params.id);
+    const deletedGroup = await groupsDAO.deleteGroup(groupId);
+    if (deletedGroup) {
+      res.json(deletedGroup);
+    } else {
+      res.status(404).json({ error: 'Group not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,30 +1,33 @@
-require('dotenv').config();
-const express = require("express");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
-
-const cookieSession = require('cookie-session');
-app.use(cookieSession({
-  name: 'session',
-  keys: [process.env.SESSION_SECRET_KEY]
-}));
+const cookieSession = require('cookie-session')
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: [process.env.SESSION_SECRET_KEY]
+  })
+)
 
 //------------------------------------
-const cors = require("cors");
-app.use(cors());
+const cors = require('cors')
+app.use(cors())
 //------------------------------------
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const userlevelsRoutes = require('./routes/userlevels-api')
 const usersRoutes = require('./routes/users-api')
+const groupsRoutes = require('./routes/groups-api')
 
 //create endpoints
-app.use('/api/userlevels',userlevelsRoutes);
-app.use('/api/users',usersRoutes);
+app.use('/api/userlevels', userlevelsRoutes)
+app.use('/api/users', usersRoutes)
+app.use('/api/groups', groupsRoutes)
 
-app.listen(process.env.PORT || 8080,()=>{
-  console.log("Server running");
-});
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Server running')
+})
