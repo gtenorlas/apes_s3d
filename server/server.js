a256fce7d99f7f8fe881bd6492623bf5d6c4be25
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-11-16 11:01:16 -0500

    create warsDAO.js

--- server/server.js
+++ server/server.js
@@ -22,11 +22,13 @@ app.use(bodyParser.json())
 const userlevelsRoutes = require('./routes/userlevels-api')
 const usersRoutes = require('./routes/users-api')
 const groupsRoutes = require('./routes/groups-api')
+const profilesRoutes = require('./routes/profiles-api');
 
 //create endpoints
 app.use('/api/userlevels', userlevelsRoutes)
 app.use('/api/users', usersRoutes)
 app.use('/api/groups', groupsRoutes)
+app.use('/api/profiles',profilesRoutes)
 
 app.listen(process.env.PORT || 8080, () => {
   console.log('Server running')
