be860fdcb81746b671bf95cab3437f23e510d2ff
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-11-17 12:16:08 -0500

    implement CRUD in wars-api.js

---
+++ server/routes/wars-api.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const WarsDAO = require('./DAO'); // Replace with the path to your WarsDAO module
+const router = express.Router();
+
+// POST route to CREATE a new war entry
+router.post('/', async (req, res) => {
+  try {
+    const newWar = await WarsDAO.createWar(req.body);
+    res.status(201).json(newWar);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// GET route to READ all war entries
+router.get('/', async (req, res) => {
+  try {
+    const allWars = await WarsDAO.getAllWars();
+    res.json(allWars);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// GET route to READ a single war entry by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const war = await WarsDAO.getWarById(req.params.id);
+    if (!war) {
+      return res.status(404).send('War not found');
+    }
+    res.json(war);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// PUT route to UPDATE a war entry
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedWar = await WarsDAO.updateWar(req.params.id, req.body);
+    if (!updatedWar) {
+      return res.status(404).send('War not found');
+    }
+    res.json(updatedWar);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// DELETE route for soft DELETING a war entry
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedWar = await WarsDAO.deleteWar(req.params.id);
+    if (!deletedWar) {
+      return res.status(404).send('War not found');
+    }
+    res.json({ message: 'War deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -23,12 +23,14 @@ const userlevelsRoutes = require('./routes/userlevels-api')
 const usersRoutes = require('./routes/users-api')
 const groupsRoutes = require('./routes/groups-api')
 const profilesRoutes = require('./routes/profiles-api');
+const warsRoutes = require('./routes/wars-api');
 
 //create endpoints
 app.use('/api/userlevels', userlevelsRoutes)
 app.use('/api/users', usersRoutes)
 app.use('/api/groups', groupsRoutes)
 app.use('/api/profiles',profilesRoutes)
+app.use('/api/wars',warsRoutes);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log('Server running')
